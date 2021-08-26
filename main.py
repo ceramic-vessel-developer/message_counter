980e1ff57314d56e280f1218e3442ec62f4844e6
ceramic-vessel-developer <speedeplaygames@gmail.com>
2021-08-26 03:07:54 +0200

    screen1 and screen2 are working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import  PyQt5
 from PyQt5 import QtWidgets
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QStackedWidget, QDialog
-import sys
+import sys,os
 import script
 name=''
 path=''
@@ -17,10 +17,48 @@ class Screen1(QDialog):
         name=self.name.text()
         if not name:
             self.error.setHidden(False)
+        else:
+            self.error.setHidden(True)
+            widget.setCurrentIndex(widget.currentIndex()+1)
+class Screen2(QDialog):
+    def __init__(self):
+        super(Screen2,self).__init__()
+        loadUi('screen_2.ui',self)
+        self.error.setHidden(True)
+        self.previous_button.clicked.connect(self.previous_clicked)
+        self.next_button.clicked.connect(self.next_clicked)
+        self.browse_button.clicked.connect(self.browse_clicked)
+    def previous_clicked(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+    def next_clicked(self):
+        path=self.line_path.text()
+        if not path:
+            self.error.setHidden(False)
+        else:
+            self.error.setHidden(True)
+            print(path)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+    def browse_clicked(self):
+        pathname=QtWidgets.QFileDialog.getExistingDirectory(self,'Choose directory',os.getcwd())
+        self.line_path.setText(pathname)
+class Screen3(QDialog):
+    def __init__(self):
+        super(Screen3,self).__init__()
+        loadUi('screen_3.ui',self)
+        self.previous_button.clicked.connect(self.previous_clicked)
+    def previous_clicked(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+
+
+
 app=QApplication(sys.argv)
 screen_1=Screen1()
+screen_2=Screen2()
+screen_3=Screen3()
 widget=QStackedWidget()
 widget.addWidget(screen_1)
+widget.addWidget(screen_2)
+widget.addWidget(screen_3)
 widget.setFixedWidth(400)
 widget.setFixedHeight(300)
 widget.show()
