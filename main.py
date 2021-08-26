dd70e524240cab738093fbcada5fddd88c830381
ceramic-vessel-developer <speedeplaygames@gmail.com>
2021-08-26 18:46:53 +0200

    finishing screen3

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import  PyQt5
-from PyQt5 import QtWidgets
+import PyQt5.QtGui
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QStackedWidget, QDialog
-import sys,os
-import script
+import sys,os,script,test,time
 name=''
 path=''
+class EmittingStream(QtCore.QObject):
+    text_written=QtCore.pyqtSignal(str)
+    def write(self,text):
+        self.text_written.emit(str(text))
 class Screen1(QDialog):
     def __init__(self):
         super(Screen1,self).__init__()
@@ -31,36 +34,54 @@ class Screen2(QDialog):
     def previous_clicked(self):
         widget.setCurrentIndex(widget.currentIndex()-1)
     def next_clicked(self):
+        global path
         path=self.line_path.text()
+
         if not path:
             self.error.setHidden(False)
         else:
-            self.error.setHidden(True)
             print(path)
+            self.error.setHidden(True)
             widget.setCurrentIndex(widget.currentIndex() + 1)
+
     def browse_clicked(self):
         pathname=QtWidgets.QFileDialog.getExistingDirectory(self,'Choose directory',os.getcwd())
         self.line_path.setText(pathname)
-class Screen3(QDialog):
-    def __init__(self):
-        super(Screen3,self).__init__()
-        loadUi('screen_3.ui',self)
-        self.previous_button.clicked.connect(self.previous_clicked)
-    def previous_clicked(self):
-        widget.setCurrentIndex(widget.currentIndex()-1)
 
 
 
 app=QApplication(sys.argv)
 screen_1=Screen1()
 screen_2=Screen2()
-screen_3=Screen3()
+
 widget=QStackedWidget()
 widget.addWidget(screen_1)
 widget.addWidget(screen_2)
+class Screen3(QDialog):
+    def __init__(self):
+        super(Screen3,self).__init__()
+        loadUi('screen_3.ui',self)
+        self.processingLabel.setHidden(True)
+        self.progressBar.setHidden(True)
+        self.pathLabel.setHidden(False)
+
+        self.process_button.clicked.connect(self.process_clicked)
+        self.previous_button.clicked.connect(self.previous_clicked)
+        widget.currentChanged.connect(self.label_update)
+    def label_update(self):
+        self.nameLabel.setText(name)
+        self.nameLabel.adjustSize()
+        self.pathLabel.setText(path)
+        self.pathLabel.adjustSize()
+    def previous_clicked(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+    def process_clicked(self):
+        pass
+screen_3=Screen3()
 widget.addWidget(screen_3)
 widget.setFixedWidth(400)
 widget.setFixedHeight(300)
+
 widget.show()
 try:
     sys.exit(app.exec_())
